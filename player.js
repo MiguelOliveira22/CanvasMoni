b767adcf472e7b8668e927b4bc4da4e975f56156
MiguelQueiroz <miguelqueiroz360@gmail.com>
2024-08-21 08:18:33 -0300

    arrumei direction

--- player.js
+++ player.js
@@ -14,11 +14,11 @@ class Player{
     draw(ctx){
         if(this.direction == 0){
             ctx.scale(-1, 1);
-            ctx.drawImage(this.sprites.sheet, this.sprites.spriteArray[0][0][0], this.sprites.spriteArray[0][0][1], this.sprites.spriteArray[0][1][0], this.sprites.spriteArray[0][1][1], -this.personagemPos.x, this.personagemPos.y, (this.personagemPos.x + this.sprites.wSprites), (this.personagemPos.y + this.sprites.aSprites));
+            ctx.drawImage(this.sprites.sheet, this.sprites.spriteArray[0][0][0], this.sprites.spriteArray[0][0][1], this.sprites.spriteArray[0][1][0], this.sprites.spriteArray[0][1][1], -this.personagemPos.x, this.personagemPos.y, -this.sprites.wSprites, this.sprites.aSprites);
         }
         else{
             ctx.scale(1, 1);
-            ctx.drawImage(this.sprites.sheet, this.sprites.spriteArray[0][0][0], this.sprites.spriteArray[0][0][1], this.sprites.spriteArray[0][1][0], this.sprites.spriteArray[0][1][1], this.personagemPos.x, this.personagemPos.y, (this.personagemPos.x + this.sprites.wSprites), (this.personagemPos.y + this.sprites.aSprites));
+            ctx.drawImage(this.sprites.sheet, this.sprites.spriteArray[0][0][0], this.sprites.spriteArray[0][0][1], this.sprites.spriteArray[0][1][0], this.sprites.spriteArray[0][1][1], this.personagemPos.x, this.personagemPos.y, this.sprites.wSprites, this.sprites.aSprites);
         }
     }
 
@@ -43,5 +43,14 @@ class Player{
     update(){
         this.nextX = this.personagemPos.x + (this.personagemPos.taxaX * this.velo);
         this.nextY = this.personagemPos.y + (this.personagemPos.taxaY * this.velo);
+        
+        if(this.nextX > this.personagemPos.x){
+            this.direction = 1;
+        }
+        else if(this.nextX < this.personagemPos.x){
+            this.direction = 0;
+        }
+
+        this.personagemPos.x = this.nextX;
     }
 }

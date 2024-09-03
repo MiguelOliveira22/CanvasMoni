e848bd42c9165f24d24f52f9b4e2f5c4748c69b3
pato14123 <henriqueinoue1000@gmail.com>
2024-09-03 11:46:58 -0300

    movimento do inimigo
    teste de movimento do inimigo
    valores fixos para posição
    ainda não possui colisão

--- JS/inimigo.js
+++ JS/inimigo.js
@@ -6,5 +6,36 @@ class Inimigo extends Collision{
         this.gravity = 2;
         this.direction = true;
         this.collided = false;
+        this.enemyPos = {
+            x:1000,
+            y:300,
+        }
+        this.directionMov = false
+    }
+
+    mov(){
+        if(this.directionMov){
+            this.enemyPos.x += this.velo
+        }
+
+        if(!this.directionMov){
+            this.enemyPos.x -= this.velo
+        }
+    }
+
+    draw(ctx){
+        this.sprites.clockVal();
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.fillRect(this.enemyPos.x, this.enemyPos.y, 60, 60)
+        ctx.stroke()
+    }
+
+    changeDirection(){
+        if(this.enemyPos.x == 0){
+            this.directionMov = true
+        }
+        if(this.enemyPos.x == 1300){
+            this.directionMov = false
+        }
     }
 }
